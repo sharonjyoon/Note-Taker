055f4de22672b48e6f6c77e58a5b620699945012
Sharon Yoon <sharonjyoon@gmail.com>
2022-04-16 23:38:52 -0700

    added changes to post

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express')
-let notes = require('./db/db.json')
-const { uid }= require('uid')
-const app = express()
-const path = require('path')
+const express = require('express');
+let notes = require('./db/db.json');
+const { uid }= require('uid');
+const app = express();
+const path = require('path');
+const fs = require('fs');
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-
+app.use(express.json());
 
 
 app.get('/', (req, res) => {
@@ -28,21 +28,24 @@ app.post('/api/notes', (req, res) => {
   let newNote = {
     title: req.body.title,
     text: req.body.text,
-    id:uid()
+    id: uid()
   }
+  //saves to db
   notes.push(newNote)
-  res.json(200)
+  fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes), err => {
+    if (err) { console.log(err) }
+    res.json(newNote)
+  })
 })
 
-//enables to delete notes
+//enables user to delete notes
 app.delete('/api/notes/:id', (req, res) => {
   notes = notes.filter(note => note.id !== req.params.id)
-  res.json(notes)
+  fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes), err => {
+    if (err) { console.log(err) }
+    res.sendStatus(200)
+  })
 })
-
-
-
-
 
 
 
